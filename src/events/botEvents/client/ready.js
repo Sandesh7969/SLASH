3004d6d10f5222ef40af90870717268da9711de5
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-01-31 20:57:29 +0545

    Update ready.js

--- src/events/botEvents/client/ready.js
+++ src/events/botEvents/client/ready.js
@@ -36,7 +36,7 @@ module.exports.run = async (client) => {
             },
             { 
               type: ActivityType.Playing, 
-              name: "TEC"
+              name: "Music"
             }
         ];
 
